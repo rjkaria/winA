cf820c107ce4ce51737d84c720cbfdb2fb02a3c6
Raju Karia <rjkaria@bigpond.com>
2016-12-08 07:21:12 +1100

    Initial commit

---
+++ today.ps1
@@ -0,0 +1,13 @@
+#!powershell
+# <license>
+
+# WANT_JSON
+# POWERSHELL_COMMON
+
+# code goes here, reading in stdin as JSON and outputting JSON
+""
+"Date / Format   YYYYMMDD        DD-MM-YYYY        MM/DD/YYYY"
+"============================================================"
+"Yesterday       " + (get-date (get-date).AddDays(-1) -uformat %Y%m%d) + "        " + (get-date (get-date).AddDays(-1) -uformat %d-%m-%Y) + "        " + (get-date (get-date).AddDays(-1) -uformat %m/%d/%Y)
+"Today           " + (get-date -uformat %Y%m%d)                        + "        " + (get-date (get-date)             -uformat %d-%m-%Y) + "        " + (get-date (get-date)             -uformat %m/%d/%Y)
+"Tomorrow        " + (get-date (get-date).AddDays(1)  -uformat %Y%m%d) + "        " + (get-date (get-date).AddDays(1)  -uformat %d-%m-%Y) + "        " + (get-date (get-date).AddDays(1)  -uformat %m/%d/%Y)
